17602b7929e544e342c1ffa6fc2f03540b132131
Kassudin <kasper.kinnarinen@live.fi>
2024-09-22 20:10:47 +0300

    Show if the user is IN or OUT
    for the upcoming events

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
 from db import db
 from sqlalchemy import text
+import users
 
 def create_event(event_type, event_date, event_start_time, event_end_time, event_location, event_description):
     sql = text("INSERT INTO events (event_type, event_date, event_start_time, event_end_time, event_location, event_description) VALUES (:event_type, :event_date, :event_start_time, :event_end_time, :event_location, :event_description)")
@@ -14,15 +15,18 @@ def create_event(event_type, event_date, event_start_time, event_end_time, event
     return True
 
 def get_list():
-    result = db.session.execute(text("""
+    sql = text("""
         SELECT e.id, e.event_type, e.event_date, e.event_start_time, e.event_end_time, e.event_location, e.event_description,
         COUNT(CASE WHEN er.status = 'IN' THEN 1 END) AS in_count,
-        COUNT(CASE WHEN er.status = 'OUT' THEN 1 END) AS out_count
+        COUNT(CASE WHEN er.status = 'OUT' THEN 1 END) AS out_count,
+        (SELECT status FROM event_registrations WHERE event_id = e.id AND user_id = :user_id) AS current_status                             
         FROM events e
         LEFT JOIN event_registrations er ON e.id = er.event_id
-        GROUP BY e.event_date, e.event_start_time, e.id
-    """))
-    return result.fetchall()
+        GROUP BY e.id
+        ORDER BY e.event_date ASC, e.event_start_time ASC
+    """)
+    result = db.session.execute (sql, {"user_id" : users.user_id()})
+    return result.fetchall() 
 
 def register_user_to_event(event_id, user_id, status):
     sql = text ("INSERT INTO event_registrations (event_id, user_id, status) VALUES (:event_id, :user_id, :status) ON CONFLICT (event_id, user_id) DO UPDATE SET status = EXCLUDED.status")
