2e592ff1597e77490fdb288ac2ef984af7a103f1
Kassudin <kasper.kinnarinen@live.fi>
2024-09-22 17:14:34 +0300

    Add new table
    To save information about users participating in events

--- schema.sql
+++ schema.sql
@@ -15,5 +15,10 @@ CREATE TABLE events (
     event_location TEXT NOT NULL,
     event_description TEXT
 );
---Add coach user. Username: Valkku, password: 1234
+CREATE TABLE event_registrations (
+    user_id INTEGER REFERENCES users(id),
+    event_id INTEGER REFERENCES events(id),
+    status TEXT,
+    UNIQUE (event_id, user_id)
+);
 INSERT INTO users (player_name, password, coach) VALUES ('Valkku', 'scrypt:32768:8:1$SAsGarfZzJqWEniE$de5ae17c0683a98ffdb26527a7d666b98225c701860893b9c8b38a4b01a8a3a2915053cc3ac677c084a68d976b27222b256c696659daeb0bd3cc46b07432ac87', TRUE); 
