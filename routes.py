137e8a4b3fe4fc0043908896e2b89b0062a80d1f
Kassudin <kasper.kinnarinen@live.fi>
2024-10-19 19:37:21 +0300

    Clean routes.py code

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
-from app import app
-from flask import render_template, request, redirect, session, abort
-import users, events, messages
 import datetime
 import secrets
+from flask import render_template, request, redirect, session, abort
+from app import app
+import users
+import events 
+import messages
 
 # Route to front page
 @app.route("/")
@@ -16,20 +18,19 @@ def index():
     user_id = users.user_id()
     event_registration_count, total_events = events.get_registration_count(user_id), events.get_total_events(user_id)
     welcome_message = f"Hei {users.player_name(user_id)}, olet ilmoittautunut {event_registration_count}/{total_events} tapahtumaan!"
-    return render_template("index.html", events = event_list, welcome_message=welcome_message)   
+    return render_template("index.html", events=event_list, welcome_message=welcome_message)   
       
 # Route to login page 
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    if request.method == "POST":
-        player_name = request.form["player_name"]
-        password = request.form["password"]
-        if users.login(player_name, password):
-            session["csrf_token"] = secrets.token_hex(16) 
-            return redirect("/")
-        return render_template("error.html", message="Väärä tunnus tai salasana")
+    player_name = request.form["player_name"]
+    password = request.form["password"]
+    if users.login(player_name, password):
+        session["csrf_token"] = secrets.token_hex(16) 
+        return redirect("/")
+    return render_template("error.html", message="Väärä tunnus tai salasana")
                 
 # Route to logout user
 @app.route("/logout")
@@ -42,35 +43,31 @@ def logout():
 def register():
     if request.method == "GET":
         return render_template("register.html")
-    if request.method == "POST":
-        player_name = request.form["player_name"]
-        password1= request.form["password1"]
-        password2= request.form["password2"]
-        player_position=request.form["player_position"]
-        player_number=request.form["player_number"]
-        if password1!=password2:
-            return render_template("error.html", message="Salasanat eroavat")
-        if not 3 <= len(player_name) <= 30:
-            return render_template("error.html", message="Virheellinen nimi")
-        allowed_positions = ["maalivahti", "puolustaja", "keskikenttä", "hyökkääjä"]
-        if player_position not in allowed_positions:
-            return render_template("error.html", message="Virheellinen pelipaikka")
-        if not player_number.isdigit() or not (1 <= int(player_number) <= 99):
-            return render_template("error.html", message="Pelaajanumeron tulee olla luku väliltä 1-99")
-        if not users.is_number_available(player_number):
-            return render_template("error.html", message="Pelinumero on jo käytössä")
-        if users.register(player_name, password1, player_position, player_number):
-            return redirect("/")
-        return render_template("error.html", message="Tapahtui virhe rekisteröitymisessä")
+    player_name = request.form["player_name"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    player_position = request.form["player_position"]
+    player_number = request.form["player_number"]
+    if password1 != password2:
+        return render_template("error.html", message="Salasanat eroavat")
+    if not 3 <= len(player_name) <= 30:
+        return render_template("error.html", message="Virheellinen nimi")
+    allowed_positions = ["maalivahti", "puolustaja", "keskikenttä", "hyökkääjä"]
+    if player_position not in allowed_positions:
+        return render_template("error.html", message="Virheellinen pelipaikka")
+    if not player_number.isdigit() or not 1 <= int(player_number) <= 99:
+        return render_template("error.html", message="Pelaajanumeron tulee olla luku väliltä 1-99")
+    if not users.is_number_available(player_number):
+        return render_template("error.html", message="Pelinumero on jo käytössä")
+    if users.register(player_name, password1, player_position, player_number):
+        return redirect("/")
+    return render_template("error.html", message="Tapahtui virhe rekisteröitymisessä")
 
 # Route to event creation page
 @app.route("/new_event")
 def new():
-    allow = False
-    if users.is_coach():
-        allow = True
-    if not allow:
-        return render_template ("error.html", message="Ei oikeutta nähdä sivua")
+    if not users.is_coach():
+        return render_template ("error.html", message="Ei oikeutta nähdä sivua")    
     return render_template("new_event.html")
 
 # Route to create a new event
@@ -120,7 +117,6 @@ def event_registration():
     return redirect("/")
 
 # Route to add a comment
-@app.route("/add_comment", methods=["POST"])
 def add_comment():
     if not users.user_id():
         return redirect("/login")
@@ -156,29 +152,27 @@ def send_message_form():
 # Route to send a new message
 @app.route("/send_message", methods=["POST"])
 def send_message():
-    if not users.user_id():
-        return redirect('/login')
     if not users.is_coach():
         return render_template("error.html", message="Vain valmentajat voivat lähettää viestejä.")
     if session["csrf_token"] != request.form["csrf_token"]:
         abort(403)
-    content = request.form.get("content")
+    content = request.form["content"]
     if not 1 <= len(content) <= 100:
         return render_template("error.html", message="Viestin tulee olla 1-100 merkkiä pitkä")
     success = messages.send_message(content)
     if success:
         return redirect("/messages")
-    else:
-        return render_template("error.html", message="Viestin lähetys epäonnistui.")
+    return render_template("error.html", message="Viestin lähetys epäonnistui.")
 
 # Route to view the messages 
 @app.route("/messages")
 def view_messages():
     if not users.user_id():
-        return redirect('/login')
+        return redirect("/login")
     message_list = messages.get_all_messages()
     return render_template("messages.html", messages=message_list)
 
+# Route to cancel the event
 @app.route("/cancel_event/<int:event_id>", methods=["POST"])
 def cancel_event(event_id):
     if not users.is_coach():
@@ -187,9 +181,9 @@ def cancel_event(event_id):
         abort(403)
     if events.cancel_event(event_id):
         return redirect("/")
-    else:
-        return render_template("error.html", message="Tapahtui virhe peruuttamisessa")
+    return render_template("error.html", message="Tapahtui virhe peruuttamisessa")
 
+# Route to edit the event
 @app.route("/edit_event/<int:event_id>")
 def edit_event(event_id):
     if not users.is_coach():
@@ -197,15 +191,15 @@ def edit_event(event_id):
     event = events.get_event_id(event_id)
     if event:
         return render_template("edit_event.html", event=event)
-    else:
-        return render_template("error.html", message= "Tapahtumaa ei löydy")
+    return render_template("error.html", message= "Tapahtumaa ei löydy")
 
+# Route to update the event
 @app.route("/update_event/<int:event_id>", methods=["POST"])
 def update_event(event_id):
     if not users.is_coach():
         return render_template("error.html", message="Ei oikeutta nähdä sivua")
     if session["csrf_token"] != request.form["csrf_token"]:
-            abort(403)
+        abort(403)
     event_type = request.form.get("event_type")
     event_date_str = request.form['event_date']
     event_date = datetime.datetime.strptime(event_date_str, '%Y-%m-%d').date()
@@ -230,6 +224,7 @@ def update_event(event_id):
     events.update_event_db(event_id, event_type, event_date, event_start_time, event_end_time, event_location, event_description, position_specific)
     return redirect("/")
 
+# Route to add an external player to the event
 @app.route("/add_external_player_to_event", methods=["POST"])
 def add_external_player_to_event():
     if not users.is_coach():
